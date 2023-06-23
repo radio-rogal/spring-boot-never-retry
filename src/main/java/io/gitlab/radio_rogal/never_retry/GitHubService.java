99ea6ff05fb9958bf7b58919aa84430cb57f69ef
Vitalij Berdinskih <vitalij_r2@outlook.com>
2023-06-23 23:07:35 +0300

    Tuning of Logging

--- src/main/java/io/gitlab/radio_rogal/never_retry/GitHubService.java
+++ src/main/java/io/gitlab/radio_rogal/never_retry/GitHubService.java
@@ -1,5 +1,7 @@
 package io.gitlab.radio_rogal.never_retry;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -7,6 +9,7 @@ import org.springframework.stereotype.Component;
 public class GitHubService implements CommandLineRunner {
 
   private static final String ACCEPT = "application/vnd.github+json";
+  private static final Logger LOGGER = LoggerFactory.getLogger(GitHubService.class);
   private static final String VERSION = "2022-11-28";
 
   private final GitHub service;
@@ -17,6 +20,8 @@ public class GitHubService implements CommandLineRunner {
 
   @Override
   public void run(String... args) {
+    LOGGER.info("owner {}, repository {}", args[0], args[1]);
+
     var contributors = service.contributors(ACCEPT, VERSION, args[0], args[1]);
 
     System.out.println(contributors.getJSONObject(0).toString(2));
